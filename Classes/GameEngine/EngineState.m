6224e7d9be0cfa89747c077e957e4a6c4c6a5d24
Jon Sharkey <jgs@sharkable.com>
2012-02-20 22:14:33 -0800

    Small style fixes.

--- Classes/GameEngine/EngineState.m
+++ Classes/GameEngine/EngineState.m
@@ -27,30 +27,30 @@
   [super dealloc];
 }
 
-- (void) stateIsShown {
+- (void)stateIsShown {
 }
 
-- (void) update {
-  for (id <StateEntity> entity in entities_) {
+- (void)update {
+  for (id<StateEntity> entity in entities_) {
     [entity update];
   }
 }
 
-- (void) render {
-  for (id <StateEntity> entity in entities_) {
+- (void)render {
+  for (id<StateEntity> entity in entities_) {
     [entity render];
   }
 }
 
-- (void) addEntity:(id <StateEntity>)entity {
+- (void) addEntity:(id<StateEntity>)entity {
   [entities_ addObject:entity];
 }
 
-- (void) removeEntity:(id <StateEntity>)entity {
+- (void) removeEntity:(id<StateEntity>)entity {
   [entities_ removeObject:entity];
 }
 
-- (void) touchesBegan:(Touch*[])touches numTouches:(int)numTouches {
+- (void) touchesBegan:(Touch *[])touches numTouches:(int)numTouches {
   for (NSObject<StateEntity> *entity in entities_) {
     if ([entity respondsToSelector:@selector(touchesBegan:numTouches:)]) {
       [entity touchesBegan:touches numTouches:numTouches];
@@ -58,7 +58,7 @@
   }
 }
 
-- (void) touchesMoved:(Touch*[])touches numTouches:(int)numTouches {
+- (void) touchesMoved:(Touch *[])touches numTouches:(int)numTouches {
   for (NSObject<StateEntity> *entity in entities_) {
     if ([entity respondsToSelector:@selector(touchesMoved:numTouches:)]) {
       [entity touchesMoved:touches numTouches:numTouches];
@@ -66,7 +66,7 @@
   }
 }
 
-- (void) touchesEnded:(Touch*[])touches numTouches:(int)numTouches {
+- (void) touchesEnded:(Touch *[])touches numTouches:(int)numTouches {
   for (NSObject<StateEntity> *entity in entities_) {
     if ([entity respondsToSelector:@selector(touchesEnded:numTouches:)]) {
       [entity touchesEnded:touches numTouches:numTouches];
