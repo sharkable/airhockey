dd2f9ac063f33b8a79b74cd8bd31f65730912c2c
Jon Sharkey <jgs@sharkable.com>
2012-04-07 19:51:52 -0700

    Made GameEngine be C++. Pulled out timing and EAGLView classes to iOS only code.

---
+++ Classes/GameEngine/ViewController.mm
@@ -0,0 +1,13 @@
+//
+//  ViewController.m
+//  AirHockey
+//
+//  Created by Jonathan Sharkey on 4/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@end
