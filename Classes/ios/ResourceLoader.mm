d3af1bdcd9549ec812a563b9fa0a44cd2cb79684
Jon Sharkey <sharkey@sharkey-macbookpro.local>
2012-04-29 22:52:40 -0300

    Fixed up formatting.

--- Classes/ios/ResourceLoader.mm
+++ Classes/ios/ResourceLoader.mm
@@ -14,72 +14,62 @@
 #include "opengl/texture2D.h"
 
 Texture2D ResourceLoader::Texture(string name) {
-  UIImage *uiImage;
-  NSUInteger        width,
-  height,
-  i;
-  CGContextRef      context = nil;
-  void*          data = nil;;
-  CGColorSpaceRef      colorSpace;
-  void*          tempData;
-  unsigned int*      inPixel32;
-  unsigned short*      outPixel16;
-  BOOL          hasAlpha;
-  CGImageAlphaInfo    info;
-  CGAffineTransform    transform;
-  SGSize          imageSize;
-  Texture2DPixelFormat    pixelFormat;
-  CGImageRef        image;
-  BOOL          sizeToFit = NO;
+  BOOL sizeToFit = NO;
   
-  
-
+  NSString *imageName = nil;
   if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-    uiImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@.png", TypeUtil::string2NSString(name)]];
+    imageName = [NSString stringWithFormat:@"%@.png", TypeUtil::string2NSString(name)];
   } else {
-    uiImage = [UIImage imageNamed:[NSString stringWithFormat:@"%@_iphone.png", TypeUtil::string2NSString(name)]];
+    imageName = [NSString stringWithFormat:@"%@_iphone.png", TypeUtil::string2NSString(name)];
   }
+  UIImage *uiImage = [UIImage imageNamed:imageName];
   
-  image = [uiImage CGImage];
+  CGImageRef image = [uiImage CGImage];
   
-  if(image == NULL) {
-    //[self release];
+  if (image == NULL) {
     image = [[UIImage imageNamed:@"yellowright.png"] CGImage];
     NSLog(@"Image is Null");
-    //return nil;
+    return nil;
+  }
+    
+  CGImageAlphaInfo info = CGImageGetAlphaInfo(image);
+  BOOL hasAlpha = ((info == kCGImageAlphaPremultipliedLast) ||
+                   (info == kCGImageAlphaPremultipliedFirst) ||
+                   (info == kCGImageAlphaLast) ||
+                   (info == kCGImageAlphaFirst) ? YES : NO);
+  Texture2DPixelFormat pixelFormat;
+
+  if (CGImageGetColorSpace(image)) {
+    if(hasAlpha) {
+      pixelFormat = kTexture2DPixelFormat_RGBA8888;
+    } else {
+      pixelFormat = kTexture2DPixelFormat_RGB565;
+    }
+  } else {  // NOTE: No colorspace means a mask image
+    pixelFormat = kTexture2DPixelFormat_A8;
   }
   
+  SGSize imageSize = SGSizeMake(CGImageGetWidth(image), CGImageGetHeight(image));
+  CGAffineTransform transform = CGAffineTransformIdentity;
   
-  info = CGImageGetAlphaInfo(image);
-  hasAlpha = ((info == kCGImageAlphaPremultipliedLast) || (info == kCGImageAlphaPremultipliedFirst) || (info == kCGImageAlphaLast) || (info == kCGImageAlphaFirst) ? YES : NO);
-  if(CGImageGetColorSpace(image)) {
-    //if(hasAlpha)
-    pixelFormat = kTexture2DPixelFormat_RGBA8888;
-    //else
-    //  pixelFormat = kTexture2DPixelFormat_RGB565;
-  } else  //NOTE: No colorspace means a mask image
-    pixelFormat = kTexture2DPixelFormat_A8;
-  
-  
-  imageSize = SGSizeMake(CGImageGetWidth(image), CGImageGetHeight(image));
-  transform = CGAffineTransformIdentity;
-  
-  width = imageSize.width;
-  
-  if((width != 1) && (width & (width - 1))) {
-    i = 1;
-    while((sizeToFit ? 2 * i : i) < width)
+  NSUInteger width = imageSize.width;
+
+  if ((width != 1) && (width & (width - 1))) {
+    NSUInteger i = 1;
+    while ((sizeToFit ? 2 * i : i) < width) {
       i *= 2;
+    }
     width = i;
   }
-  height = imageSize.height;
-  if((height != 1) && (height & (height - 1))) {
-    i = 1;
-    while((sizeToFit ? 2 * i : i) < height)
+  NSUInteger height = imageSize.height;
+  if ((height != 1) && (height & (height - 1))) {
+    NSUInteger i = 1;
+    while ((sizeToFit ? 2 * i : i) < height) {
       i *= 2;
+    }
     height = i;
   }
-  while((width > kMaxTextureSize) || (height > kMaxTextureSize)) {
+  while ((width > kMaxTextureSize) || (height > kMaxTextureSize)) {
     width /= 2;
     height /= 2;
     transform = CGAffineTransformScale(transform, 0.5, 0.5);
@@ -87,17 +77,23 @@ Texture2D ResourceLoader::Texture(string name) {
     imageSize.height *= 0.5;
   }
   
+  CGContextRef context = NULL;
+  void *data = NULL;
+  CGColorSpaceRef colorSpace = NULL;
+
   switch(pixelFormat) {    
     case kTexture2DPixelFormat_RGBA8888:
       colorSpace = CGColorSpaceCreateDeviceRGB();
       data = malloc(height * width * 4);
-      context = CGBitmapContextCreate(data, width, height, 8, 4 * width, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+      context = CGBitmapContextCreate(data, width, height, 8, 4 * width, colorSpace,
+                                      kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
       CGColorSpaceRelease(colorSpace);
       break;
     case kTexture2DPixelFormat_RGB565:
       colorSpace = CGColorSpaceCreateDeviceRGB();
       data = malloc(height * width * 4);
-      context = CGBitmapContextCreate(data, width, height, 8, 4 * width, colorSpace, kCGImageAlphaNoneSkipLast | kCGBitmapByteOrder32Big);
+      context = CGBitmapContextCreate(data, width, height, 8, 4 * width, colorSpace,
+                                      kCGImageAlphaNoneSkipLast | kCGBitmapByteOrder32Big);
       CGColorSpaceRelease(colorSpace);
       break;
       
@@ -113,16 +109,20 @@ Texture2D ResourceLoader::Texture(string name) {
   CGContextClearRect(context, CGRectMake(0, 0, width, height));
   CGContextTranslateCTM(context, 0, height - imageSize.height);
   
-  if(!CGAffineTransformIsIdentity(transform))
+  if (!CGAffineTransformIsIdentity(transform)) {
     CGContextConcatCTM(context, transform);
-  CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image), CGImageGetHeight(image)), image);
+  }
+  CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image), CGImageGetHeight(image)),
+                     image);
   //Convert "RRRRRRRRRGGGGGGGGBBBBBBBBAAAAAAAA" to "RRRRRGGGGGGBBBBB"
-  if(pixelFormat == kTexture2DPixelFormat_RGB565) {
-    tempData = malloc(height * width * 2);
-    inPixel32 = (unsigned int*)data;
-    outPixel16 = (unsigned short*)tempData;
-    for(i = 0; i < width * height; ++i, ++inPixel32)
-      *outPixel16++ = ((((*inPixel32 >> 0) & 0xFF) >> 3) << 11) | ((((*inPixel32 >> 8) & 0xFF) >> 2) << 5) | ((((*inPixel32 >> 16) & 0xFF) >> 3) << 0);
+  if (pixelFormat == kTexture2DPixelFormat_RGB565) {
+    void *tempData = malloc(height * width * 2);
+    unsigned int *inPixel32 = (unsigned int*)data;
+    unsigned short *outPixel16 = (unsigned short*)tempData;
+    for (NSUInteger i = 0; i < width * height; ++i, ++inPixel32)
+      *outPixel16++ = ((((*inPixel32 >> 0) & 0xFF) >> 3) << 11) |
+                      ((((*inPixel32 >> 8) & 0xFF) >> 2) << 5) |
+                      ((((*inPixel32 >> 16) & 0xFF) >> 3) << 0);
     free(data);
     data = tempData;
     
